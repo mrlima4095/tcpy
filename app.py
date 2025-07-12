79ae8c6c602046c825d8f66c5ad341e17e1bcbef
Mr Lima <felipebr4095@gmail.com>
2025-07-12 20:40:09 -0300

    Improving messages

--- app.py
+++ app.py
@@ -8,14 +8,29 @@ class Application:
         self.server.bind((host, port))
         self.server.listen(31522)
         
-        print(f" * Running Remote Python at port {port}")
+        print(f"[+] Listening at port {port}")
 
         while True:
             client_sock, addr = server.accept()
             thread = threading.Thread(target=handle_client, args=(client_sock, addr))
             thread.start()
     def handle_client(client_socket, addr):
-        print(f"[+] Conexão de {addr}")
+        print(f"[+] {addr} connected")
+        try:
+            while True:
+                payload = client_socket.recv(4096).decode('utf-8')
+                if not payload:
+                    break
+                print(f"[>] Executando:\n{payload}")
+                try:
+                    exec(payload)
+                except Exception as e:
+                    print(f"[!] Erro ao executar: {e}")
+        finally:
+            print(f"[-] Conexão encerrada: {addr}")
+            client_socket.close()
+    def handle_client(client_socket, addr):
+        print(f"[+] {addr} connected")
         try:
             while True:
                 payload = client_socket.recv(4096).decode('utf-8')
